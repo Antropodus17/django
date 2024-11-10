e2dad14bda5d057c6d71cb16cb90b0f679a89819
AntropodusKali <sergioparis02@yahoo.com>
2024-11-10 21:59:31 +0100

    Intento de uso de los models en django

--- code/entrega_formulario/forms/views.py
+++ code/entrega_formulario/forms/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, "index.html")
+
+
+def form(request):
+    return render(request, "form.html")
+
+
+def validateForm(request):
+    from models import userLogin
+    import json
+
+    username = request.GET.get("username")
+    password = request.GET.get("password")
+    web_server = request.GET.get("webServer")
+    role = request.GET.get("role")
+    mail = request.GET.get("mail")
+    payroll = request.GET.get("payroll")
+    self_service = request.GET.get("selfService")
+    try:
+        user = userLogin(
+            username,
+            password,
+            web_server,
+            role,
+            {"mail": mail, "payroll": payroll, "selfService": self_service},
+        )
+
+        return render(request, "index.html", {"userLogin": user})
+    except Exception as e:
+        return render(request, "form.html", {"error": e})
