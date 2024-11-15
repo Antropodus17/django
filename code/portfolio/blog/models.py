1fcdca4a8ddc3501df196ae1d370354349951a0c
a23sergiopn <a23sergiopn@iessanclemente.net>
2024-11-15 11:52:51 +0100

    Cambio campo fecha blog

--- code/portfolio/blog/models.py
+++ code/portfolio/blog/models.py
@@ -6,4 +6,4 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    fecha = models.DateField()
+    fecha = models.DateField(auto_now_add=True)
