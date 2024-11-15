aaa33f37004bdd0823594cd078f0a0db1819e532
a23sergiopn <a23sergiopn@iessanclemente.net>
2024-11-15 12:54:28 +0100

    Querys

--- code/portfolio/blog/views.py
+++ code/portfolio/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
@@ -6,4 +6,15 @@ from .models import Blog
 
 def all_blogs(request):
 
-    return render(request, "index-blog.html", context={"blogs": Blog.objects.all()})
+    return render(
+        request,
+        "index-blog.html",
+        context={"blogs": Blog.objects.all().order_by("fecha")[:2]},
+    )
+
+
+def detail(request, blog_id):
+
+    blog = get_object_or_404(Blog, pk=blog_id)
+
+    return render(request, "details-blog.html", {"blog": blog})
--- code/portfolio/portfolio/settings.py
+++ code/portfolio/portfolio/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import sys
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, "appsDir"))
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,10 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATICFILES_DIRS = [
+    BASE_DIR / STATIC_URL,
+]
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / MEDIA_URL
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
