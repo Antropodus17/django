56558466d80cc88256e66fdb8eeb96a331826149
Antropodus17_Kali <sergioparis02@gmail.com>
2025-03-09 12:45:02 +0100

    Auto commit generated by git_commit_all.sh dom 09 mar 2025 12:45:02 CET

--- code/satisfactory/apps/objects/admin.py
+++ code/satisfactory/apps/objects/admin.py
@@ -7,10 +7,18 @@ from .models import Recipe
 
 class ResourceAdmin(admin.ModelAdmin):
     list_display = ["name"]
+    list_filter = (
+        "name",
+        "messure",
+    )
 
 
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ["craft_name"]
+    list_filter = (
+        "id",
+        "id_craft_resource",
+    )
 
     @admin.display(ordering="resource__name")
     def craft_name(self, obj: Recipe):
